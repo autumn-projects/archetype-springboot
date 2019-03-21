55c3c536ed678f7265db18b7fbdc1d102bed6dc4
liaowang <524342291@qq.com>
2019-03-21 14:51:04 +0800

    swagger2

--- src/main/java/com/oscroll/controller/DemoController.java
+++ src/main/java/com/oscroll/controller/DemoController.java
@@ -2,20 +2,24 @@ package com.oscroll.controller;
 
 import com.oscroll.entity.DemoEntity;
 import com.oscroll.service.DemoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
+@Api(value = "Demo", tags = {"实例"})
 @Controller
 public class DemoController {
 
     @Autowired
     private DemoService demoService;
 
-    @RequestMapping("/find")
+    @ApiOperation(value = "查找")
+    @GetMapping("/find")
     @ResponseBody
     public List<DemoEntity> findAll(){
         return demoService.findAll();
