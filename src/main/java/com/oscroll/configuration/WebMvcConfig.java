7640cbf5efa848e404f13f51c4aaeed990e8501e
liaowang <524342291@qq.com>
2019-07-04 16:28:11 +0800

    访问静态资源兼容swagger-ui

--- src/main/java/com/oscroll/configuration/WebMvcConfig.java
+++ src/main/java/com/oscroll/configuration/WebMvcConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
 
 }
